3bfdb360fbe75d06b09ca0b915102bb1d5009b30
Rik Rosseel <contact@rikrosseel.com>
2025-11-05 16:12:28 +0100

    Add theme to Card.tsx

--- ski-resort-management/components/Card.tsx
+++ ski-resort-management/components/Card.tsx
@@ -6,41 +6,15 @@ import useTheme from "@components/ThemeContext";
 interface CardProps {
     readonly children?: ReactNode;
     readonly style?: StyleProp<ViewStyle>;
-    readonly marginX?: number;
-    readonly marginY?: number;
-    readonly paddingX?: number;
-    readonly paddingY?: number;
     readonly border?: boolean;
     readonly shadow?: boolean;
 }
 
-export default function Card({ children, style, marginX = 20, marginY = 20, paddingX = 20, paddingY = 20, shadow = true, border }: CardProps) {
+export default function Card({ children, style, shadow = true, border }: CardProps) {
     const theme = useTheme();
 
     return (
-        <View style={{
-            backgroundColor: '#fff',
-            marginLeft: marginX,
-            marginRight: marginX,
-            marginTop: marginY,
-            marginBottom: marginY,
-            paddingLeft: paddingX,
-            paddingRight: paddingX,
-            paddingTop: paddingY,
-            paddingBottom: paddingY,
-            borderRadius: 12,
-            ...(shadow && {
-                shadowColor: '#000',
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.1,
-                shadowRadius: 4,
-                elevation: 3,
-            }),
-            ...(border && {
-                borderWidth: 1,
-                borderColor: "#e0e0e0",
-            })
-        }}>
+        <View style={[theme.card, style]}>
             {children}
         </View>
     );
