1c387ff39c0906a9d462718963bd25a2d105efa8
Rik Rosseel <contact@rikrosseel.com>
2026-01-08 21:10:33 +0100

    Fix tabs smoke test to use firebase authentication

--- ski-resort-management/cypress/e2e/smoke-tabs.cy.js
+++ ski-resort-management/cypress/e2e/smoke-tabs.cy.js
@@ -1,4 +1,32 @@
 describe("Smoke tests - Tabs", () => {
+
+    before(() => {
+        let random = Math.random().toString(36).substring(2, 8);
+        let email = `john.doe.cypress+${random}@example.com`;
+        cy.visit("/");
+        cy.get('[data-testid="register-button"]').click();
+        cy.get('[data-testid="register-first-name"]').should("be.visible");
+        cy.get('[data-testid="register-first-name"]').type('John');
+        cy.get('[data-testid="register-last-name"]').type('Doe');
+        cy.get('[data-testid="register-email"]').type(email);
+        cy.get('[data-testid="register-password"]').type('Password123!');
+        cy.get('[data-testid="register-password-confirmation"]').type('Password123!');
+        cy.get('[data-testid="submit-button"]').click();
+        cy.wait(1000);
+        cy.visit("/slopes");
+        cy.get('[data-testid="screen-slopes"]').should("be.visible");
+    });
+
+    after(() => {
+        cy.visit("/(tabs)/")
+        cy.wait(1000);
+        cy.get('button[aria-label="Account"]').should('be.visible').click();
+        cy.wait(1000);
+        cy.get('[data-testid="logout-button"]').should('be.visible').click();
+        cy.wait(1000);
+        cy.get('[data-testid="landing-screen"]').should("be.visible");
+    });
+
     it("Home tab loads", () => {
         cy.visit("/");
         cy.get('[data-testid="screen-home"]').should("be.visible");
